7c061496e73aea4531816f089e8e57089715f7eb
chybie <chy.bie@htmail.com>
2017-02-11 16:54:05 +0800

    implement

--- README.md
+++ README.md
@@ -4,6 +4,21 @@
 
 ## Build Setup
 
+Yarn
+
+``` bash
+# install dependencies
+yarn
+
+# serve with hot reload at localhost:8080
+yarn run dev
+
+# build for production with minification
+yarn run build
+```
+
+Or if you are using NPM,
+
 ``` bash
 # install dependencies
 npm install
--- src/App.vue
+++ src/App.vue
@@ -5,73 +5,100 @@
       <form enctype="multipart/form-data" novalidate v-if="isInitial || isSaving">
         <h1>Upload images</h1>
         <div class="dropbox">
-          <input type="file" multiple :name="uploadFieldName" 
-            :disabled="isInitial"
+          <input type="file" multiple :name="uploadFieldName" :disabled="isSaving" 
+            @change="filesChange($event.target.name, $event.target.files); fileCount=$event.target.files.length"
             accept="image/*" class="input-file">
             <p v-if="isInitial">
               Drag your file(s) here to begin<br> or click to browse
             </p>
             <p v-if="isSaving">
-              Uploading files...
-              <!--{{ photos.files.length }} -->
+              Uploading {{ fileCount }} files...
             </p>
         </div>
       </form>
       <!--SUCCESS-->
-      <div class="margin-20" v-if="isSuccess">
+      <div v-if="isSuccess">
         <h2>Uploaded {{ uploadedFiles.length }} file(s) successfully.</h2>
         <p>
-          <a href="javascript:void(0)" >Upload again</a>
+          <a href="javascript:void(0)" @click="reset()">Upload again</a>
         </p>
         <ul class="list-unstyled">
-          <li *ngFor="let item of uploadedFiles">
+          <li v-for="item in uploadedFiles">
             <img :src="item.url" class="img-responsive img-thumbnail" :alt="item.originalName">
           </li>
         </ul>
       </div>
       <!--FAILED-->
-      <div class="margin-20" v-if="isFailed">
+      <div v-if="isFailed">
         <h2>Uploaded failed.</h2>
         <p>
-          <a href="javascript:void(0)" >Try again</a>
+          <a href="javascript:void(0)" @click="reset()">Try again</a>
         </p>
-        <!--<pre>{{ uploadError | json }}</pre>-->
+        <pre>{{ uploadError }}</pre>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import * as axios from 'axios'
+
+  const STATUS_INITIAL = 0, STATUS_SAVING = 1, STATUS_SUCCESS = 2, STATUS_FAILED = 3;
+  const BASE_URL = 'http://localhost:3001';
+
+  // utils to delay promise
+  const wait = function (ms) {
+    return function (x) {
+      return new Promise(resolve => setTimeout(() => resolve(x), ms));
+    };
+  }
+
   export default {
     name: 'app',
     mounted() {
-
+      this.reset();
     },
     computed: {
       isInitial() {
-        return this.currentStatus === this.STATUS_INITIAL;
+        return this.currentStatus === STATUS_INITIAL;
       },
       isSaving() {
-        return this.currentStatus === this.STATUS_SAVING;
+        return this.currentStatus === STATUS_SAVING;
       },
       isSuccess() {
-        return this.currentStatus === this.STATUS_SUCCESS;
+        return this.currentStatus === STATUS_SUCCESS;
       },
       isFailed() {
-        return this.currentStatus === this.STATUS_FAILED;
+        return this.currentStatus === STATUS_FAILED;
       }
     },
     methods: {
       reset() {
-        this.currentStatus = this.STATUS_INITIAL;
+        // reset form to initial state
+        this.currentStatus = STATUS_INITIAL;
         this.uploadedFiles = [];
         this.uploadError = null;
       },
       save(formData) {
         // upload data to the server
-        this.currentStatus = this.STATUS_SAVING;
+        this.currentStatus = STATUS_SAVING;
+        const url = `${BASE_URL}/photos/upload`;
+        axios.post(url, formData)
+          .then(wait(1500))
+          .then(x => x.data)
+          .then(x => {
+            const result = x.map(img => Object.assign({},
+              img, { url: `${BASE_URL}/images/${img.id}` }));
+            this.uploadedFiles = [].concat(result);
+            this.currentStatus = STATUS_SUCCESS;
+          })
+          .catch(err => {
+            this.uploadError = err.response;
+            this.currentStatus = STATUS_FAILED;
+          })
       },
       filesChange(fieldName, fileList) {
+        console.log(fieldName, fileList)
         // handle file changes
         const formData = new FormData();
 
@@ -90,12 +117,6 @@
     },
     data() {
       return {
-        // status
-        STATUS_INITIAL: 0,
-        STATUS_SAVING: 1,
-        STATUS_SUCCESS: 2,
-        STATUS_FAILED: 3,
-
         uploadedFiles: [],
         uploadError: null,
         currentStatus: null,
